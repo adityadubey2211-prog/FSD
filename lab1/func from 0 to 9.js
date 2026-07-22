14701ee1c745290073a531c368e58c81b68bfa10
adityadubey2211-prog <adityadubey2211@gamil.com>
2026-07-22 11:18:11 +0530

    second commit

---
+++ lab1/func from 0 to 9.js
@@ -0,0 +1,42 @@
+const num = (num) =>{
+    if(num == 1){
+        console.log("one");
+    }
+    else if(num == 2){
+        console.log("two");
+    }
+    else if(num == 3){
+        console.log("three");
+    }
+    else if(num == 4){
+        console.log("four");
+    }
+    else if(num == 5){
+        console.log("five");
+    }
+    else if(num == 6){
+        console.log("six");
+    }
+    else if(num == 7){
+        console.log("seven");
+    }
+    else if(num == 8){
+        console.log("eight");
+    }
+    else if(num == 9){
+        console.log("nine");
+    }
+    else{
+        console.log("please enter a valid num");
+    }
+}
+num(1);
+num(2);
+num(3);
+num(4);
+num(5);
+num(6);
+num(7);
+num(8);
+num(9);
+num(10);
